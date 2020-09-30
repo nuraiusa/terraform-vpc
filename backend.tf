a43ffb5b82c28a76ac6a27ca354c21d51e1cc271
nuraiusa <69086090+nuraiusa@users.noreply.github.com>
2020-09-30 12:11:46 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "nuraiusa"
+        bucket = "nuraiusa2"
         key = "terraform_vpc/us-east-1/class/dev/infrastructure.tfstate"
         region = "us-east-1"
     }
